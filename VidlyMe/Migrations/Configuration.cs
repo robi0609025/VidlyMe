22ec115807bba969e9d1db0f6b5e2d10295336e2
robi0609025 <robi.kuet@gmail.com>
2019-05-18 01:05:55 +1000

    Add migration
    Up to Lecture 25

---
+++ VidlyMe/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace VidlyMe.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VidlyMe.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(VidlyMe.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- VidlyMe/Models/IdentityModels.cs
+++ VidlyMe/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace VidlyMe.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }    //Customer dbset is added
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
