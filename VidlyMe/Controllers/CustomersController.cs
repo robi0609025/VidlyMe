e69a9a95a91d990a7c8127a738d0622e94a95782
robi0609025 <robi.kuet@gmail.com>
2019-05-22 23:58:59 +1000

    End of section 3

--- VidlyMe/Controllers/CustomersController.cs
+++ VidlyMe/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,17 +10,29 @@ namespace VidlyMe.Controllers
 {
     public class CustomersController : Controller
     {
-        // GET: Customers
+        //start - context class is used for accessing database. It needs to be disposed.
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        //end - context class is used for accessing database. It needs to be disposed.
+
         public ActionResult Index()
         {
-            var Customers = GetCustomer();
+            //var Customers = GetCustomer();
+            var Customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(Customers);
         }
 
         public ActionResult Details(int? id)
         {
-            var Customer = GetCustomer().SingleOrDefault(c => c.CustomerId == id);
+            var Customer = _context.Customers.SingleOrDefault(c => c.CustomerId == id);
 
             if (Customer == null)
             {
@@ -31,15 +44,15 @@ namespace VidlyMe.Controllers
 
 
         //Get Customer method to collect customer data
-        private IEnumerable<Customer> GetCustomer()
-        {
-            var Customers = new List<Customer>()
-            {
-                new Customer(){Name = "John Smith", CustomerId = 1 },
-                new Customer(){Name = "Mary William", CustomerId = 2 }
-            };
-            return Customers;
-        }
+        //private IEnumerable<Customer> GetCustomer()
+        //{
+        //    var Customers = new List<Customer>()
+        //    {
+        //        new Customer(){Name = "John Smith", CustomerId = 1 },
+        //        new Customer(){Name = "Mary William", CustomerId = 2 }
+        //    };
+        //    return Customers;
+        //}
 
     }
 }
---
+++ VidlyMe/Migrations/201905211323285_AddMembershipType.cs
@@ -0,0 +1,34 @@
+namespace VidlyMe.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.MembershipTypes",
+                c => new
+                    {
+                        MembershipTypeId = c.Byte(nullable: false),
+                        SignUpFee = c.Short(nullable: false),
+                        DurationInMonth = c.Byte(nullable: false),
+                        DiscountRate = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.MembershipTypeId);
+            
+            AddColumn("dbo.Customers", "MembershipTypeId", c => c.Byte(nullable: false));
+            CreateIndex("dbo.Customers", "MembershipTypeId");
+            AddForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes", "MembershipTypeId", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes");
+            DropIndex("dbo.Customers", new[] { "MembershipTypeId" });
+            DropColumn("dbo.Customers", "MembershipTypeId");
+            DropTable("dbo.MembershipTypes");
+        }
+    }
+}
